7cf15a56ea17a1ee96119e850cda52809f9ce795
Marcin <marcin.konow@lczyk.xyz>
2024-10-18 23:18:35 +0100

    v bump

--- src/environ_get/environ_get_parser.py
+++ src/environ_get/environ_get_parser.py
@@ -21,7 +21,7 @@ else:
     TypeGuard = type
 
 # NOTE: This mirrors the version in environ_get.py. Remember to update both. There is a test which checks this.
-__version__ = "0.3.0"
+__version__ = "1.0.0"
 
 __all__ = ["generate_environ_doc"]
 
